011042758c4340301286d221c72441f16c59e288
Anthony <anthonyzhu132@gmail.com>
2019-10-10 23:40:13 +0000

    ex6 was to use strings and utilizing how we can use it, ex11 was to use gets.chomp

---
+++ ex6.rb
@@ -0,0 +1,15 @@
+types_of_people = 10 #Listing the number of people
+x = "there are #{types_of_people} types of people" #Creating a string with the number of people
+binary = "binary" 
+do_not = "dont"
+y = "Those who know #{binary} and those who #{do_not}" #Using a string to see tell whether or not people understand binary
+puts x
+puts y
+puts "I said: #{x}"
+puts "I also said: '#{y}''."
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+puts joke_evaluation
+w = "THis is the left side of ..."
+e = "a string with a right side"
+puts w + e
