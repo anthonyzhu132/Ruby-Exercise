a15f230d9127f593b03a3d80ee8bc5384e7d73fd
Anthony <anthonyzhu132@gmail.com>
2019-10-10 22:38:07 +0000

    first commit for ruby exercises

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# A Comment, this is so you can read your program later.
+# Anything after # is ingored by Ruby.
+
+puts "I could have code like this." #And the comment after is ignored
+
+# You cna also use a comment to "disable" or comment a piece of code
+
+puts "this will run"
